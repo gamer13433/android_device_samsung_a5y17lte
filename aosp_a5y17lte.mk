e44c2ec6e5249f8c0c3ffe8ec79e354efd445c99
Lars <61460660+larsb24@users.noreply.github.com>
2020-03-31 20:04:38 +0200

    Modify for AOSP-ten

--- aosp_a5y17lte.mk
+++ aosp_a5y17lte.mk
@@ -24,18 +24,36 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_a5y17lte
+PRODUCT_NAME := aosp_a5y17lte
 PRODUCT_DEVICE := a5y17lte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A520F
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Fingerprint
+BUILD_FINGERPRINT := "samsung/a5y17ltexx/a5y17lte:8.0.0/R16NW/A520FXXU8CSB5:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="a5y17ltexx-user 8.0.0 R16NW A520FXXU8CSB5 release-keys" \
+    PRODUCT_NAME="a5y17lte" \
+    TARGET_DEVICE="a5y17lte"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+# Vendor security patch
+VENDOR_SECURITY_PATCH := 2020-01-01
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
